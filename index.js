792ae6c4f3ccd3815349b55828ecf7250d08ea03
D.Giridhar <143996339+girigiridhar03@users.noreply.github.com>
2024-01-31 13:26:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+let date = document.querySelector("#date");
+const button =document.querySelector("button");
+const para = document.querySelector("p");
+
+button.addEventListener("click",()=>{
+     let birthdayDate = new Date(date.value);
+
+     let d1 = birthdayDate.getDate();
+     let m1 = birthdayDate.getMonth()+1;
+     let y1 = birthdayDate.getFullYear();
+
+     let todayDate = new Date();
+   
+     let d2 = todayDate.getDate();
+     let m2 = todayDate.getMonth() +1;
+     let y2 = todayDate.getFullYear();
+
+     let d3,m3,y3;
+
+     y3 = y2-y1;
+
+     if(m2>m2){
+         m3 = m2-m1;
+     }
+     else{
+        y3--;
+        m3 = 12+m2-m1;
+     }
+     if(d2>d1){
+        d3 = d2-d1;
+     }
+     else{
+        m3--;
+        d3 = getDaysinMonths(y1,m1)+d2-d1;
+     }
+
+    if(m3 < 0){
+        m3 =11;
+        y3--
+    }
+   if(date.value == ""){
+    para.innerHTML = "invalid";
+    para.style.color = "red";
+   }
+   else{
+       if(m3 === 1){
+        para.innerHTML = `${y3} years ${m3}month ${d3} days old`;
+        para.style.color = "green";
+       }
+       else{
+        para.innerHTML = `${y3} years ${m3}months ${d3} days old`;
+        para.style.color = "green";
+       }
+
+   }
+   
+});
+
+
+function getDaysinMonths(year,month){
+     return new Date(year,month,0).getDate()
+}
